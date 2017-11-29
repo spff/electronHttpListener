b2a8c05566ef175b78a2a6adb3488050f94ea046
spff <spff@bitmessage.ch>
2017-11-30 00:56:10 +0800

    demo, have some bugs

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const electron = require('electron')
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
+const ipc = electron.ipcMain
+const Menu = electron.Menu
+const Tray = electron.Tray
+
 
 const path = require('path')
 const url = require('url')
@@ -14,6 +18,8 @@ let mainWindow
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow.setMenuBarVisibility(false)
+
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -22,16 +28,112 @@ function createWindow () {
     slashes: true
   }))
 
+
+
+
+  function put_in_tray(event) {
+    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png'
+    const iconPath = path.join(__dirname, iconName)
+    appIcon = new Tray(iconPath)
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: 'Remove',
+        click: function () {
+          event.sender.send('tray-removed')
+        }
+      },
+      {
+        label: 'Exit',
+        click: function () {
+          event.sender.send('tray-exit')
+        }
+      }
+    ])
+    appIcon.setToolTip('Electron Demo in the tray.')
+    appIcon.setContextMenu(contextMenu)
+
+    mainWindow.hide()
+  }
+
+  function remove_tray() {
+    appIcon.destroy()
+    mainWindow.show()
+
+  }
+
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
+
+
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
+
+  // Dereference the window object, usually you would store windows
+  // in an array if your app supports multi windows, this is the time
+  // when you should delete the corresponding element.
+  mainWindow.on('closed', function(){
     mainWindow = null
   })
+
+
+
+  mainWindow.on('close', function (event){
+    console.log("on close")
+
+    put_in_tray(event)
+    event.preventDefault()
+  })
+
+  mainWindow.on('minimize', put_in_tray)
+
+
+	let appIcon = null
+
+	ipc.on('put-in-tray', put_in_tray)
+
+	ipc.on('remove-tray', remove_tray)
+
+
+  ipc.on('exit-tray', function () {
+    app.quit()
+    
+  })
+
+
+  ipc.on("console", function (ev) {
+        var args = [].slice.call(arguments, 1);
+        var r = console.log.apply(console, args)
+        ev.returnValue = [r];
+  });
+  ipc.on("app", function (ev, msg) {
+      var args = [].slice.call(arguments, 2);
+      ev.returnValue = [app[msg].apply(app, args)]
+  });
+  
+
+  var window = new BrowserWindow({show: false})
+  window.loadURL("file://" + __dirname + "/app.html")
+  window.webContents.once("did-finish-load", function () {
+      var http = require("http");
+      var crypto = require("crypto");
+
+
+      const server = http.createServer((req, res) => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('okay');
+        console.log("get")
+        remove_tray()
+      });
+      
+      // now that proxy is running
+      server.listen(1337, '127.0.0.1');
+
+      console.log("http://localhost:1337/")
+  })
+
+
+
+
 }
 
 // This method will be called when Electron has finished
@@ -43,6 +145,9 @@ app.on('ready', createWindow)
 app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
+  if (appIcon)
+    appIcon.destroy()
+
   if (process.platform !== 'darwin') {
     app.quit()
   }
@@ -58,3 +163,5 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+
